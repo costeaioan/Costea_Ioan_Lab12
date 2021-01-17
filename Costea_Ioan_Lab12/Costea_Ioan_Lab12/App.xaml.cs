5e5d891c6031c6c10bf6da753fc3f62105356ba3
ioan <costeaioan23@gmail.com>
2021-01-17 13:12:31 +0200

    Add project files.

---
+++ Costea_Ioan_Lab12/Costea_Ioan_Lab12/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Costea_Ioan_Lab12.Data;
+
+namespace Costea_Ioan_Lab12
+{
+    public partial class App : Application
+    {
+        public static ShoppingListDatabase Database { get; private set; }
+        public App()
+        {
+            Database = new ShoppingListDatabase(new RestService());
+            MainPage = new NavigationPage(new ListEntryPage());
+
+            //InitializeComponent();
+
+            //MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
